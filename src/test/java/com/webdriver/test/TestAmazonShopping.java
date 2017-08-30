03d2140f9bf0cde5e23a5fbfb4ff85f6ac47fcca
padma <padhub4u@gmail.com>
2017-08-29 22:47:35 -0700

    Adding comments to the test

--- src/test/java/com/webdriver/test/TestAmazonShopping.java
+++ src/test/java/com/webdriver/test/TestAmazonShopping.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
-import org. openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * Class to search and add items to shopping cart in Amazon.com
@@ -45,18 +45,27 @@ public class TestAmazonShopping {
      */
     @Test
     public void testAddItemToCart(){
+        // 1. Set search category to "Books"
         WebElement category = wd.findElement(By.id("searchDropdownBox"));
         new Select(category).selectByVisibleText("Books");
+
+        // 2. Set search keyword to "Selenium" and hit "Go"
         WebElement text = wd.findElement(By.id("twotabsearchtextbox"));
         text.sendKeys("Selenium");
         WebElement goButton = wd.findElement(By.xpath("//*[@value='Go']"));
         goButton.click();
+
+        // 3. Find the first search result and drill-in
         WebElement firstBook = wd.findElement(By.className("s-access-title"));
         String firstBookName = firstBook.getText();
         firstBook.click();
+
+        // 4. Add the item to shopping cart
         assert (wd.findElement(By.id("productTitle")).getText().equals(firstBookName));
         wd.findElement(By.id("add-to-cart-button")).click();
         assert (wd.findElement(By.xpath(".//*[@id='huc-v2-order-row-messages']")).getText().equals("Added to Cart"));
+
+        // 5. Open shopping cart and verify that item got added correctly
         wd.findElement(By.className("a-button")).click();
         assert (wd.findElement(By.className("a-list-item")).getText().contains(firstBookName));
     }
